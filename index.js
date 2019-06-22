1ee44eb19e1302a4cae31dacfd3f461a68a51390
Oskar Dahlin <oskardahlin10@hotmail.com>
2019-06-22 18:54:17 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-console.log("Loading tensorflow model...")
-
-const model = await tf.loadLayersModel('model/model.json');
-
-const example = tf.fromPixels(webcamElement);  // for example
-const prediction = model.predict(example);
