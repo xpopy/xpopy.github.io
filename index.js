a70f8a9c3f3514dddfefee76a0f8da7ae234ceea
Oskar Dahlin <oskardahlin10@hotmail.com>
2019-06-22 18:49:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+console.log("It WORKS!")
+
+
+/*
+import * as tf from '@tensorflow/tfjs';
+
+const model = await tf.loadLayersModel('https://foo.bar/tfjs_artifacts/model.json');
+
+const example = tf.fromPixels(webcamElement);  // for example
+const prediction = model.predict(example);
+*/
